77cd97c14e0ccfeba628963add93e265f91ddb3f
kajenthini <kajakajenthini1999@gmail.com>
2022-09-12 15:18:36 +0530

    Final Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./i18n";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Biography from "./components/Biography";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="font-system flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-1">
+        <Hero />
+        <Biography />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
